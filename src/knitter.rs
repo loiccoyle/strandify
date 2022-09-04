c187dd465a41fac7ba05062408c6b861f5277c88
Loic Coyle <loic.coyle@hotmail.fr>
2022-09-04 14:30:12 +0200

    implement line computation and initial sketch of algorithm

--- src/knitter.rs
+++ src/knitter.rs
@@ -1,4 +1,5 @@
 use image::{GrayImage, Luma};
+use std::cmp;
 use std::iter::zip;
 use std::path::PathBuf;
 
@@ -61,11 +62,14 @@ impl Knitter {
                     continue;
                 }
 
-                let (line_x, line_y) = last_peg.line_to(peg, &self.yarn);
+                let (line_x, line_y) = last_peg.line_to(peg);
                 let line_pixels: Vec<u8> = zip(line_x, line_y)
                     .map(|(x, y)| self.image[(x, y)].0[0])
                     .collect();
-                let pixel_sum: f64 = line_pixels.iter().sum::<u8>() as f64;
+                // TODO: check if this overflows
+                let pixel_sum: f64 = line_pixels
+                    .iter()
+                    .fold(0.0, |acc, &pixel| acc + pixel as f64);
                 if pixel_sum < min {
                     min = pixel_sum;
                     min_peg = Some(&peg);
@@ -100,8 +104,48 @@ impl Peg {
         Self { x, y, id }
     }
     /// Get the pixel coords connecting 2 pegs.
-    pub fn line_to(&self, other_peg: &Peg, yarn: &Yarn) -> (Vec<u32>, Vec<u32>) {
-        unimplemented!()
+    pub fn line_to(&self, other_peg: &Peg) -> (Vec<u32>, Vec<u32>) {
+        let delta_x: i64 = (i64::from(self.x) - i64::from(other_peg.x)).abs();
+        let delta_y: i64 = (i64::from(self.y) - i64::from(other_peg.y)).abs();
+
+        let line_fn;
+        if delta_x >= delta_y {
+            line_fn = self.line_x_fn_to(other_peg);
+            let x_coords = cmp::min(self.x, other_peg.x)..(cmp::max(self.x, other_peg.x) + 1);
+            let y_coords: Vec<u32> = x_coords
+                .clone()
+                .map(line_fn)
+                .map(|y| y.round() as u32)
+                .collect();
+            return (x_coords.collect(), y_coords);
+        } else {
+            line_fn = self.line_y_fn_to(other_peg);
+            let y_coords = cmp::min(self.y, other_peg.y)..(cmp::max(self.y, other_peg.y) + 1);
+            let x_coords: Vec<u32> = y_coords
+                .clone()
+                .map(line_fn)
+                .map(|x| x.round() as u32)
+                .collect();
+            return (x_coords, y_coords.collect());
+        }
+    }
+
+    fn get_line_coefs(&self, other_peg: &Peg) -> (f64, f64) {
+        let slope: f64 = (f64::from(other_peg.y) - f64::from(self.y))
+            / (f64::from(other_peg.x) - f64::from(self.x));
+        let intercept: f64 = f64::from(self.y) - slope * f64::from(self.x);
+        // TODO: how to handle the case where the line is vertical
+        (slope, intercept)
+    }
+
+    fn line_x_fn_to(&self, other_peg: &Peg) -> Box<dyn FnMut(u32) -> f64> {
+        let (slope, intercept) = self.get_line_coefs(other_peg);
+        Box::new(move |x| slope * f64::from(x) + intercept)
+    }
+
+    fn line_y_fn_to(&self, other_peg: &Peg) -> Box<dyn FnMut(u32) -> f64> {
+        let (slope, intercept) = self.get_line_coefs(other_peg);
+        Box::new(move |y| f64::from(y) / slope - intercept / slope)
     }
 }
 
@@ -114,5 +158,46 @@ pub struct Yarn {
 impl Yarn {
     pub fn new(width: u8, opacity: f32) -> Self {
         Self { width, opacity }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_get_line_coefs() {
+        let peg_a = Peg::new(0, 0, 0);
+        let peg_b = Peg::new(1, 1, 1);
+        let (slope, intercept) = peg_a.get_line_coefs(&peg_b);
+        assert_eq!(slope, 1.);
+        assert_eq!(intercept, 0.);
+
+        let peg_a = Peg::new(1, 1, 0);
+        let peg_b = Peg::new(0, 1, 1);
+        let (slope, intercept) = peg_a.get_line_coefs(&peg_b);
+        assert_eq!(slope, 0.);
+        assert_eq!(intercept, 1.);
+    }
+
+    #[test]
+    fn test_line_to() {
+        let peg_a = Peg::new(0, 0, 0);
+        let peg_b = Peg::new(1, 1, 1);
+        let (x_coords, y_coords) = peg_a.line_to(&peg_b);
+        assert_eq!(x_coords, vec![0, 1]);
+        assert_eq!(y_coords, vec![0, 1]);
+
+        let peg_a = Peg::new(1, 1, 0);
+        let peg_b = Peg::new(0, 0, 1);
+        let (x_coords, y_coords) = peg_a.line_to(&peg_b);
+        assert_eq!(x_coords, vec![0, 1]);
+        assert_eq!(y_coords, vec![0, 1]);
+
+        let peg_a = Peg::new(0, 1, 0);
+        let peg_b = Peg::new(3, 1, 1);
+        let (x_coords, y_coords) = peg_a.line_to(&peg_b);
+        assert_eq!(x_coords, vec![0, 1, 2, 3]);
+        assert_eq!(y_coords, vec![1, 1, 1, 1]);
     }
 }
