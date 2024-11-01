c363686e0cda4ea2992ff8e7152dca8c192fb7c5
Loic Coyle <loic.coyle@hotmail.fr>
2024-11-01 03:49:31 +0100

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
+  base: "/strandify/",
   plugins: [react(), wasm(), topLevelAwait()],
   worker: { plugins: () => [wasm(), topLevelAwait()], format: "es" },
 });
